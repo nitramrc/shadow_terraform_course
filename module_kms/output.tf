cd02d12883a1a374a3c5f1d09e29358ba383c1d3
nitramrc <martin.horejsek@gmail.com>
2022-03-01 03:07:57 +0100

    add terraform aws/kms module

---
+++ module_kms/output.tf
@@ -0,0 +1,8 @@
+
+output "alias" {
+  value = var.alias
+}
+
+output "id" {
+  value = concat(aws_kms_key.this.*.id, [""])[0]
+}
---
+++ output.tf
@@ -0,0 +1,8 @@
+
+output "key_alias" {
+  value = module.kms.alias
+}
+
+output "key_arn" {
+  value = module.kms.id
+}
